2a418f4be21d7fc91a9cd31d31771ba1511b8d40
hamza-4li <sy.hamza.4li@gmail.com>
2025-02-28 22:43:29 +0500

    note screen added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,40 @@
 import { Stack } from "expo-router";
 
 const RootLayout = () => {
-  return <Stack />;
-}
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "orange",
+        },
+        headerTintColor: "#fff",
+        headerTitleAlign: "center",
+        headerTitleStyle: {
+          fontSize: 20,
+          fontWeight: "bold",
+        },
+        contentStyle: {
+          paddingHorizontal: 10,
+          paddingTop: 10,
+          backgroundColor: "#fff",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="notes"
+        options={{
+          headerTitle: "Notes",
+        }}
+      />
+    </Stack>
+  );
+};
 
 export default RootLayout;
---
+++ app/notes/index.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+
+const NoteScreen = () => {
+  const [notes, setNotes] = useState([
+    { id: "1", text: "Note One" },
+    { id: "2", text: "Note Two" },
+    { id: "3", text: "Note Three" },
+    { id: "4", text: "Note Four" },
+    { id: "5", text: "Note Five" },
+    { id: "6", text: "Note Six" },
+  ]);
+  return (
+    <View style={StyleSheet.container}>
+      <FlatList
+        data={notes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <View style={styles.noteItem}>
+          <Text style ={styles.noteText}>{item.text}</Text>
+        </View>}
+      />
+      <TouchableOpacity style = {styles.addButton}>
+        <Text style ={styles.addButtonText}>+ Add Note</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  noteItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#f5f5f5',
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  noteText: {
+    fontSize: 18,
+  },
+  addButton:{
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+    backgroundColor: '#007bff',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
+});
+
+export default NoteScreen;
